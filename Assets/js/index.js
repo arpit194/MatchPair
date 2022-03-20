ebf12738415f57a78136b0e2bb434d2f62804e33
arpit194 <arpit.patel194@gmail.com>
2022-03-20 13:16:08 +0530

    Add function to add cards to game container

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -34,4 +34,99 @@ $(document).ready(function () {
     $('#settings').on("click", function () {
         closeDiv('settings');
     });
+
+    //To Close pop ups
+    function closeDiv(id) {
+        $('#' + id).fadeOut("slow");
+    }
+
+    //To display pop ups
+    function showDiv(id) {
+        $('#' + id).fadeIn("slow");
+    }
+
+    //To add cards to the game container
+    function addCards() {
+        $('#row1').html("");
+        $('#row2').html("");
+        cardValues = ["python", "node-js", "java", "google", "apple", "microsoft", "python", "node-js", "java", "google", "apple", "microsoft"];
+        cardValues.sort(() => Math.random() - 0.5);
+        cardDict = {};
+        for (let i = 0; i < 6; i++) {
+            let temp = cardValues.pop();
+            cardDict["1" + i] = temp;
+            //Append cards to the first row
+            $('#row1').append(`
+                <div class="cardContainer">
+                    <div class="gameCard" id="1` + i + temp + `">
+                        <div class="card__face card__face--front"></div>
+                        <div class="card__face card__face--back"><i class="fa-brands fa-`+ temp + `"></i></div>
+                    </div>
+                </div>
+            `);
+            temp = cardValues.pop();
+            cardDict["2" + i] = temp;
+
+            //Append cards to the second row
+            $('#row2').append(`
+                <div class="cardContainer">
+                    <div class="gameCard" id="2` + i + temp + `">
+                        <div class="card__face card__face--front"></div>
+                        <div class="card__face card__face--back"><i class="fa-brands fa-`+ temp + `"></i></div>
+                    </div>
+                </div>
+            `);
+        }
+
+        //When a card is clicked
+        $('.gameCard').on("click", function () {
+            //Start Timer
+            if (!gamestarted) {
+                let interval = setInterval(() => {
+                    time--;
+                    $('#time').html(time);
+                    if (cardsMatched == 6) {
+                        clearInterval(interval);
+                        gameOver("Win");
+                        gameOver = true;
+                    }
+                    else if (time == 0) {
+                        clearInterval(interval);
+                        gameOver("Lose");
+                        gameOver = true;
+                    }
+                }, 1000);
+                gamestarted = true;
+            }
+
+            //To flip cards
+            if (flippedCount < 2 && jQuery.inArray($(this)[0].id, flippedCards) == -1 && gameOver) {
+                $(this).addClass("is-flipped");
+                flippedCards.push($(this)[0].id);
+                flippedCount++;
+                //When two cards are already flipped
+                if (flippedCount == 2) {
+                    let matched = checkMatch();
+                    if (matched) {
+                        setTimeout(() => {
+                            points++;
+                            $("#" + flippedCards[0]).addClass("is-matched");
+                            $("#" + flippedCards[1]).addClass("is-matched");
+                            flippedCards = [];
+                            flippedCount = 0;
+                            cardsMatched++;
+                        }, 500);
+                    }
+                    else {
+                        setTimeout(() => {
+                            $("#" + flippedCards[0]).removeClass("is-flipped");
+                            $("#" + flippedCards[1]).removeClass("is-flipped");
+                            flippedCards = [];
+                            flippedCount = 0;
+                        }, 1000);
+                    }
+                }
+            }
+        });
+    }
 });
