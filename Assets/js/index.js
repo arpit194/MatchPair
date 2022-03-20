6a4d0a186e55835d7ec4dae0b6ef0c306bd75dff
arpit194 <arpit.patel194@gmail.com>
2022-03-20 13:15:05 +0530

    Add variables and event listeners

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,17 +1,37 @@
 $(document).ready(function () {
+    //variables
+    let flippedCount = 0;
+    let points = 0;
+    let flippedCards = [];
+    let cardValues = [];
+    let cardDict = {};
+    let time = 30;
+    let gamestarted = false;
+    let gameOver = true;
+    let cardsMatched = 0;
+    let difficulty = "Easy";
+
+    $('#time').html(time);
+
+    //Event Handlers 
     $('.instructionBtn').on("click", function () {
         showDiv('instructions');
     });
 
+    $('.settingBtn').on("click", function () {
+        showDiv('settings');
+    });
+
     $('.close').on("click", function () {
+        closeDiv('instructions');
+        closeDiv('settings');
+    });
+
+    $('#instructions').on("click", function () {
         closeDiv('instructions');
     });
 
-    function closeDiv(id) {
-        $('#' + id).fadeOut("slow");
-    }
-
-    function showDiv(id) {
-        $('#' + id).fadeIn("slow");
-    }
+    $('#settings').on("click", function () {
+        closeDiv('settings');
+    });
 });
